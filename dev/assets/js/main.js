06fec9ef1c4605ce8537e582e5e7a6c214083566
Dedli <dedli888@gmail.com>
2018-06-19 08:44:37 +0300

    plug slider

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,116 +1,116 @@
 // countdown init script
 
-const myCountdown = new countdown({
-    target: '.countdown',
-    dayWord: ' день',
-    hourWord: ' час',
-    minWord: ' минут',
-    secWord: ' сек'
-});
-
-const myCountdown2 = new countdown({
-    target: '.countdown2',
-    dayWord: ' день',
-    hourWord: ' час',
-    minWord: ' минут',
-    secWord: ' сек'
-});
-const myCountdown3 = new countdown({
-    target: '.countdown3',
-    dayWord: ' день',
-    hourWord: ' час',
-    minWord: ' минут',
-    secWord: ' сек'
-});
+// const myCountdown = new countdown({
+//     target: '.countdown',
+//     dayWord: ' день',
+//     hourWord: ' час',
+//     minWord: ' минут',
+//     secWord: ' сек'
+// });
+//
+// const myCountdown2 = new countdown({
+//     target: '.countdown2',
+//     dayWord: ' день',
+//     hourWord: ' час',
+//     minWord: ' минут',
+//     secWord: ' сек'
+// });
+// const myCountdown3 = new countdown({
+//     target: '.countdown3',
+//     dayWord: ' день',
+//     hourWord: ' час',
+//     minWord: ' минут',
+//     secWord: ' сек'
+// });
 
 // countdown init script ends
 
 // banner script starts
 
-// banner autoplay script starts
-(function () {
-    function Slideshow(element) {
-        this.el = document.querySelector(element);
-        this.init();
-    }
-
-    Slideshow.prototype = {
-        init: function () {
-            this.slides = this.el.querySelectorAll(".mySlides");
-            this.index = 0;
-            this.timer = null;
-            this.action();
-            this.stopStart();
-        },
-        _slideTo: function (slide) {
-            var currentSlide = this.slides[slide];
-            currentSlide.style.display = "block";
-            for (var i = 0; i < this.slides.length; i++) {
-                var slide = this.slides[i];
-                if (slide !== currentSlide) {
-                    slide.style.display = "none";
-                }
-            }
-        },
-        action: function () {
-            var self = this;
-            self.timer = setInterval(function () {
-                self.index++;
-                if (self.index == self.slides.length) {
-                    self.index = 0;
-                }
-                self._slideTo(self.index);
-
-            }, 3000);
-        },
-        stopStart: function () {
-            var self = this;
-            self.el.addEventListener("mouseover", function () {
-                clearInterval(self.timer);
-                self.timer = null;
-
-            }, false);
-            self.el.addEventListener("mouseout", function () {
-                self.action();
-
-            }, false);
-        }
-
-    };
-
-    document.addEventListener("DOMContentLoaded", function () {
-
-        var banner = new Slideshow(".banner");
-
-    });
-
-})();
+// // banner autoplay script starts
+// (function () {
+//     function Slideshow(element) {
+//         this.el = document.querySelector(element);
+//         this.init();
+//     }
+//
+//     Slideshow.prototype = {
+//         init: function () {
+//             this.slides = this.el.querySelectorAll(".mySlides");
+//             this.index = 0;
+//             this.timer = null;
+//             this.action();
+//             this.stopStart();
+//         },
+//         _slideTo: function (slide) {
+//             var currentSlide = this.slides[slide];
+//             currentSlide.style.display = "block";
+//             for (var i = 0; i < this.slides.length; i++) {
+//                 var slide = this.slides[i];
+//                 if (slide !== currentSlide) {
+//                     slide.style.display = "none";
+//                 }
+//             }
+//         },
+//         action: function () {
+//             var self = this;
+//             self.timer = setInterval(function () {
+//                 self.index++;
+//                 if (self.index == self.slides.length) {
+//                     self.index = 0;
+//                 }
+//                 self._slideTo(self.index);
+//
+//             }, 3000);
+//         },
+//         stopStart: function () {
+//             var self = this;
+//             self.el.addEventListener("mouseover", function () {
+//                 clearInterval(self.timer);
+//                 self.timer = null;
+//
+//             }, false);
+//             self.el.addEventListener("mouseout", function () {
+//                 self.action();
+//
+//             }, false);
+//         }
+//
+//     };
+//
+//     document.addEventListener("DOMContentLoaded", function () {
+//
+//         var banner = new Slideshow(".banner");
+//
+//     });
+//
+// })();
 
 // banner autoplay script ends
 
 // banner buttons prev and next script
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-    var i;
-    var slidesItem = document.getElementsByClassName("mySlides");
-    if (n > slidesItem.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slidesItem.length
-    }
-    for (i = 0; i < slidesItem.length; i++) {
-        slidesItem[i].style.display = "none";
-    }
-    slidesItem[slideIndex - 1].style.display = "block";
-}
+//
+// var slideIndex = 1;
+// showSlides(slideIndex);
+//
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
+//
+// function showSlides(n) {
+//     var i;
+//     var slidesItem = document.getElementsByClassName("mySlides");
+//     if (n > slidesItem.length) {
+//         slideIndex = 1
+//     }
+//     if (n < 1) {
+//         slideIndex = slidesItem.length
+//     }
+//     for (i = 0; i < slidesItem.length; i++) {
+//         slidesItem[i].style.display = "none";
+//     }
+//     slidesItem[slideIndex - 1].style.display = "block";
+// }
 
 // banner buttons script ends
 
@@ -260,3 +260,12 @@ language.addEventListener('mouseleave', hideLanguagePanel);
 
 
 // language dropdown script ends
+
+$(document).ready(function(){
+    $('.together-cheaper-slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        autoplay: true,
+        slidesToShow: 3
+});
+});
